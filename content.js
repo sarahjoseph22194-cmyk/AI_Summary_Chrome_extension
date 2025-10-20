fe094b9610a7d967a28bf7cd6de51d1a6e0d5128
sarahjoseph22194-cmyk <sarah.joseph22194@gmail.com>
2025-10-20 19:09:40 +0300

    Initial commit

---
+++ content.js
@@ -0,0 +1,10 @@
+/* global chrome */
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: () => {
+    alert('Hello from my extension');
+        },
+    });
+});
